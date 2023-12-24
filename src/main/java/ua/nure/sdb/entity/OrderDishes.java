c32deb419e4a43be5cfbb717fa8c1d98f95f5ead
AlexBadImagination <102557936+AlexBadImagination@users.noreply.github.com>
2023-12-24 19:22:55 +0200

    All patterns used

--- src/main/java/ua/nure/sdb/entity/OrderDishes.java
+++ src/main/java/ua/nure/sdb/entity/OrderDishes.java
@@ -1,19 +1,44 @@
 package ua.nure.sdb.entity;
 
+import java.sql.Date;
+import java.sql.Time;
+
 public class OrderDishes {
     private int order;
     private int dish;
     private int amount;
     private int priority;
 
-    public OrderDishes() {
-    }
+    public static class Builder {
+        private OrderDishes newOrderDishes;
 
-    public OrderDishes(int order, int dish, int amount, int priority) {
-        this.order = order;
-        this.dish = dish;
-        this.amount = amount;
-        this.priority = priority;
+        public Builder() {
+            newOrderDishes = new OrderDishes();
+        }
+
+        public Builder withOrder(int order) {
+            newOrderDishes.order = order;
+            return this;
+        }
+
+        public Builder withDish(int dish) {
+            newOrderDishes.dish = dish;
+            return this;
+        }
+
+        public Builder withAmount(int amount) {
+            newOrderDishes.amount = amount;
+            return this;
+        }
+
+        public Builder withPriority(int priority) {
+            newOrderDishes.priority = priority;
+            return this;
+        }
+
+        public OrderDishes build() {
+            return newOrderDishes;
+        }
     }
 
     public int getOrder() {
